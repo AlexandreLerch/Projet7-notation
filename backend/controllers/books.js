482a5c91e27044752cbaff6f39546ce180ec3a57
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-07-18 15:38:07 +0200

    Verif_backend

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -2,6 +2,8 @@ const Book = require("../models/Book");
 const multer = require("multer");
 const sharp = require("sharp");
 const fs = require("fs");
+const auth = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
 
 const MIME_TYPES = {
   "image/jpg": "jpg",
@@ -52,89 +54,113 @@ exports.createBook = (req, res, next) => {
 };
 
 exports.modify = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+  const userId = decodedToken.userId;
+
   const extension = MIME_TYPES["image/webp"];
   const date = Date.now();
+
   Book.findOne({ _id: req.params.id })
     .then((book) => {
-      let bookObject;
+      console.log("créateur du livre : " + book.userId);
+      console.log("utilisateur loggé : " + userId);
 
-      if (req.file) {
-        bookObject = {
-          ...JSON.parse(req.body.book),
-          imageUrl: `${req.protocol}://${req.get("host")}/images/${
-            req.file.originalname.split(" ").join("_").split(".").shift() +
-            date +
-            "." +
-            extension
-          }`,
-        };
+      if (userId === book.userId) {
+        let bookObject;
 
-        delete bookObject._userId;
-        sharp(req.file.buffer)
-          .resize({ height: 500 })
-          .toFile(
-            `images/${
+        if (req.file) {
+          bookObject = {
+            ...JSON.parse(req.body.book),
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${
               req.file.originalname.split(" ").join("_").split(".").shift() +
               date +
               "." +
               extension
-            }`
-          )
-          .catch((error) => console.log(error));
+            }`,
+          };
 
-        const fileToDelete = book.imageUrl.split("/images/")[1];
-        fs.unlink(`./images/${fileToDelete}`, (error) => {
-          if (error) {
-            console.error(error);
-            return res.status(500).json({
-              error:
-                "Une erreur s'est produite lors de la suppression de l'image.",
-            });
-          }
-        });
+          delete bookObject._userId;
+          sharp(req.file.buffer)
+            .resize({ height: 500 })
+            .toFile(
+              `images/${
+                req.file.originalname.split(" ").join("_").split(".").shift() +
+                date +
+                "." +
+                extension
+              }`
+            )
+            .catch((error) => console.log(error));
+
+          const fileToDelete = book.imageUrl.split("/images/")[1];
+          fs.unlink(`./images/${fileToDelete}`, (error) => {
+            if (error) {
+              console.error(error);
+              return res.status(500).json({
+                error:
+                  "Une erreur s'est produite lors de la suppression de l'image.",
+              });
+            }
+          });
+        } else {
+          bookObject = { ...req.body };
+          delete bookObject._userId;
+        }
+
+        Book.updateOne(
+          { _id: req.params.id },
+          { ...bookObject, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: "Objet modifié" }))
+          .catch((error) => res.status(401).json({ error }));
       } else {
-        bookObject = { ...req.body };
-        delete bookObject._userId;
+        return res.status(403).json({ message: "Vous n'êtes pas autorisé à modifier cet objet." });
       }
-
-      Book.updateOne(
-        { _id: req.params.id },
-        { ...bookObject, _id: req.params.id }
-      )
-        .then(() => res.status(200).json({ message: "Objet modifié" }))
-        .catch((error) => res.status(401).json({ err }));
     })
     .catch((error) => res.status(400).json({ error }));
 };
 
+
+//if userId = request.auth.id   vérifier si l'utilisateur est l'id du livre si les deux sont égaux, on ne doit pas
 exports.delete = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+  const userId = decodedToken.userId;
+
   Book.findOne({ _id: req.params.id })
     .then((book) => {
       if (!book) {
         return res.status(404).json({ message: "L'objet n'a pas été trouvé." });
       }
-      console.log(book.imageUrl);
-      const fileName = book.imageUrl.split("/images/")[1];
 
-      fs.unlink(`./images/${fileName}`, (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({
-            error:
-              "Une erreur s'est produite lors de la suppression de l'image.",
-          });
-        }
-        Book.deleteOne({ _id: req.params.id })
-          .then(() =>
-            res
-              .status(200)
-              .json({ message: "L'objet a été supprimé avec succès." })
-          )
-          .catch((error) => res.status(400).json({ error }));
-      });
+      console.log("créateur du livre : " + book.userId);
+      console.log("utilisateur loggé : " + userId);
+
+      if (userId === book.userId) {
+        const fileName = book.imageUrl.split("/images/")[1];
+
+        fs.unlink(`./images/${fileName}`, (err) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({
+              error: "Une erreur s'est produite lors de la suppression de l'image.",
+            });
+          }
+
+          Book.deleteOne({ _id: req.params.id })
+            .then(() =>
+              res.status(200).json({ message: "L'objet a été supprimé avec succès." })
+            )
+            .catch((error) => res.status(400).json({ error }));
+        });
+      } else {
+        return res.status(403).json({ message: "Vous n'êtes pas autorisé à supprimer cet objet." });
+      }
     })
     .catch((error) => res.status(400).json({ error }));
 };
+
 
 exports.getOne = (req, res, next) => {
   Book.findOne({ _id: req.params.id })
@@ -149,12 +175,29 @@ exports.getAll = (req, res, next) => {
 };
 
 exports.bookRating = (req, res, next) => {
+
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+  const userId = decodedToken.userId;
+
+  Book.findOne({ _id: req.params.id })
+  .then((book) => {
+    if (!book) {
+      return res.status(404).json({ message: "L'objet n'a pas été trouvé." });
+    }
+
+    console.log("créateur du livre : " + book.userId);
+    console.log("utilisateur loggé : " + userId);
+
+  if (userId !== book.userId) {
+
   const ratingObject = req.body;
   if (ratingObject.userId) {
     ratingObject.grade = ratingObject.rating;
     delete ratingObject.rating;
 
     Book.findByIdAndUpdate(
+      
       { _id: req.params.id },
       { $push: { ratings: ratingObject } },
       { new: true }
@@ -181,6 +224,10 @@ exports.bookRating = (req, res, next) => {
   } else {
     return res.status(404).json({ message: "vous devez être connecté." });
   }
+} else {
+  return res.status(403).json({ message: "Vous n'êtes pas autorisé à renoter cet objet" });
+};
+});
 };
 
 exports.getBestRated = (req, res, next) => {
